a60e3936d25fa04cd57df5a247311b85ffae26c4
JAS0N-SMITH <huchknows@gmail.com>
2025-05-27 18:16:52 -0400

    refactor: update Home component to remove 'Skills' section from display

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center">
-        {["Profile", "Positions", "Education", "Certifications", "Skills"].map(
+        {["Profile", "Positions", "Education", "Certifications"].map(
           (sectionKey) => {
             if (!data[sectionKey]) return null;
 
